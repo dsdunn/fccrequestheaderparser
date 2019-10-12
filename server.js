d11f2c41220a95fb388cdd7b3e61e424c28dafce
dsdunn <dsdunnmusic@gmail.com>
2019-10-11 18:56:02 -0600

    fcc backend challenge #2

--- server.js
+++ server.js
@@ -20,13 +20,18 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/*", function (req, res) {
+  res.json({
+    "ip_address":req.connection.remoteAddress,
+    "language":req.headers['accept-language'],
+    "software":req.headers['user-agent']
+  });
 });
 
 
 
+let port = process.env.PORT || 3311;
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(port, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
